05e634b4ad4415ca11ae11e43c0482ba9f324b96
tanmay <tanmaysawaji9@gmail.com>
2025-08-09 01:42:48 +0530

    second commit

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 
+function App(){
+    
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render();
