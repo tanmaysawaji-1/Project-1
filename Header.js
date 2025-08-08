4102450fde88163b249029960b876155077be8f0
tanmay <tanmaysawaji9@gmail.com>
2025-08-09 02:16:19 +0530

    third commit

--- Header.js
+++ Header.js
@@ -2,10 +2,11 @@ export default function Header(){
     return(
         <header className="bg-orange-600">
             <div>
-                <h1>hello</h1>
+                <img className="w-40 h-12" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/static-assets/images/swiggy_logo_white.png"/>
             </div>
             <div></div>
             <div></div>
         </header>
+
     )
 }
--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-
+import Header from "./Header";
 
 function App(){
-    
+    return (
+
+        <>
+        <Header></Header>
+        </>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render();
+root.render(<App></App>);
