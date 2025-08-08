0dfb916c357e2a43eeb44e63ac28b008c30f024b
tanmay <tanmaysawaji9@gmail.com>
2025-08-09 02:04:39 +0530

    push Header

---
+++ Header.js
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <header className="bg-orange-600">
+            <div>
+                <h1>hello</h1>
+            </div>
+            <div></div>
+            <div></div>
+        </header>
+    )
+}
