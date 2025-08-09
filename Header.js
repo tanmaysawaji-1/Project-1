17c5f00144ad01f7f47ba6602b18a61bd4bccae6
tanmay <tanmaysawaji9@gmail.com>
2025-08-10 00:58:43 +0530

    seven commit

--- Header.js
+++ Header.js
@@ -1,18 +1,19 @@
-export default function Header(){
-    return(
-        <header className="bg-[#ff5200]">
+export default function Header() {
+    return (
+        <header className="bg-[#ff5200] p-4">
             <div className="flex">
-                <img className="w-40 h-12" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/static-assets/images/swiggy_logo_white.png"/>
-                <div className="font-serif text-white text-base font-bold">
+                <img
+                    className="w-40 h-12"
+                    src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/static-assets/images/swiggy_logo_white.png"
+                    alt="Swiggy Logo"
+                />
+                <div className="font-serif text-white text-base font-bold ">
                     <a href="https://www.swiggy.com/corporate/" target="_blank" >Swiggy Corporate</a>
-                    <a href="https://partner.swiggy.com/login#/swiggy" target="_blank">Partner with us</a>
-                    <a>Get the App</a>
-                    <a>Sign in</a>
+                    <a href="https://partner.swiggy.com/login#/swiggy" target="_blank" >Partner with us</a>
+                    <a >Get the App</a>
+                    <a >Sign in</a>
                 </div>
             </div>
-            <div></div>
-            <div></div>
         </header>
-
-    )
+    );
 }
--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import Header from "./Header";
 
+
 function App(){
     return (
         <>
