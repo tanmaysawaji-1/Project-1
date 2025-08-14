2c5ce804a1ee1327dd44fa1faa57ee7ef6803d35
tanmay <tanmaysawaji9@gmail.com>
2025-08-14 23:43:36 +0530

    12 commit

--- Restaurant.js
+++ Restaurant.js
@@ -17,6 +17,7 @@ export default function Restaurant(){
             fetchdata();
         },[])
         
+        
         return (
             <>
             <div className=" flex flex-wrap w-[90%] mx-auto mt-20 gap-5">
