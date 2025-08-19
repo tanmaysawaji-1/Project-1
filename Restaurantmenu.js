1eb3b89191bd374e6e29a33c1a5c9920ac212182
tanmay <tanmaysawaji9@gmail.com>
2025-08-20 00:01:18 +0530

    16 commit

--- Restaurantmenu.js
+++ Restaurantmenu.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router"
 export default function Restaurantmenu(){
     let id = useParams();
-    console.log(id);
+
+   const [Rdata, setdata] = useState(null);
+
+   useEffect(()=>{
+
+    async function fetchdata(){
+    const proxyserver= "https://cors-anywhere.herokuapp.com/";
+    const swiggyapi = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5211&lng=73.8502&restaurantId=${id}&catalog_qa=undefined&submitAction=ENTER`;
+    const response = await fetch(proxyserver + swiggyapi);
+    const data = await response.json();
+    setdata(data);
+    }
+
+    fetchdata();
+},[]);
+
+console.log(id);
+console.log(Rdata)
     return(
        <>
       
